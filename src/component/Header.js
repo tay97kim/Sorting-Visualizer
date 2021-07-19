afb714164766e1cb689c2bff32b917bde2b1b942
Taeseong Kim <55838882+tay97kim@users.noreply.github.com>
2021-07-20 00:11:07 +0900

    Update Header.js
    [21.07.20] Update for implementing sequential/binary search.

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Slider from '@material-ui/core/Slider';
 
-import { Button, SortButton, Container, Wrapper, ControlWrapper, SpeedWrapper, SpeedContainer, SortWrapper, CountContainer } from "./header.style";
+import { Button, Button2, Container, Wrapper, ControlWrapper, SpeedWrapper, SpeedContainer, SortWrapper, SearchWrapper, CountContainer} from "./header.style";
 import { Typography } from "@material-ui/core";
 
 var valueBtn = false;
@@ -10,30 +10,32 @@ var valueSpeed = 3;
 var valueCP = 0;
 var valueSW = 0;
 var valueMC = 0;
-var sortType = 0;
+var valueSC = 0;
+var taskType = 0;
+var sorted = 0;
 
 export function valueClear() {
     valueCP = 0;
     valueSW = 0;
     valueMC = 0;
+    valueSC = 0;
 }
 
 export function plusValue(int) {
     switch (int) {
-        case 1: {
+        case 1: 
             valueCP++;
             break;
-        }
-        case 2: {
+        case 2: 
             valueSW++;
             break;
-        }
-        case 3: {
+        case 3: 
             valueMC++;
             break;
-        }
-        default: {
-        }
+        case 4: 
+            valueSC++;
+            break;
+        default: 
     }
 }
 
@@ -45,16 +47,18 @@ export function buttonOn() {
     valueBtn = false;
 }
 
-export default function Header({ sortPause, sortRestart, setArray, updateList, rangeChange, selectClicked, bubbleClicked, insertClicked, mergeClicked, sortStart }) {
+export default function Header({ sortPause, setArray, updateList, rangeChange, selectClicked, bubbleClicked, insertClicked, mergeClicked, sequentialSearch, binarySearch, sortStart }) {
+    
     const [valueS, setValueS] = React.useState(3);
 
     const sleep = (milliseconds) => { //딜레이 메소드
         return new Promise((resolve) => setTimeout(resolve, milliseconds));
     };
 
-    const handleSliderChangeSpeed = (event, newValue) => {
+    const SliderChange = (event, newValue) => {
         setValueS(newValue);
         setSpeed(valueS);
+        document.getElementById("speedV").innerText = newValue;
     };
 
     function setSpeed(inputSpeed) {
@@ -63,19 +67,21 @@ export default function Header({ sortPause, sortRestart, setArray, updateList, r
 
     function buttonOff() {
         if (valueBtn === false) {
-            if (sortType === 0) {
+            if (taskType === 0) {
                 alert("정렬의 종류를 먼저 선택한 뒤 시작해주세요");
             }
             else {
                 valueBtn = true;
-                onSorting();
+                onTask();
                 sortStart();
+                sorted=1;
             }
         }
     }
 
     function setArrayBtn() {
         if (valueBtn === false) {
+            sorted = 0;
             valueClear();
             setArray();
         }
@@ -83,6 +89,7 @@ export default function Header({ sortPause, sortRestart, setArray, updateList, r
 
     function updateListBtn() {
         if (valueBtn === false) {
+            sorted = 0;
             valueClear();
             updateList();
         }
@@ -90,6 +97,7 @@ export default function Header({ sortPause, sortRestart, setArray, updateList, r
 
     function rangeChangeBtn() {
         if (valueBtn === false) {
+            sorted = 0;
             valueClear();
             rangeChange();
         }
@@ -97,68 +105,100 @@ export default function Header({ sortPause, sortRestart, setArray, updateList, r
 
     function select() {
         if (valueBtn === false) {
-            sortType = 1;
+            taskType = 1;
             selectClicked();
         }
     }
 
     function bubble() {
         if (valueBtn === false) {
-            sortType = 2;
+            taskType = 2;
             bubbleClicked();
         }
     }
 
     function insert() {
         if (valueBtn === false) {
-            sortType = 3;
+            taskType = 3;
             insertClicked();
         }
     }
 
     function merge() {
         if (valueBtn === false) {
-            sortType = 4;
+            taskType = 4;
             mergeClicked();
         }
     }
 
+    function sequential(){
+        if(sorted == 0){
+            alert("정렬된 배열에서 탐색을 진행할 수 있도록 원소 변경 후, 최소 한 번의 정렬을 진행해주세요.");
+        }
+        else{
+            valueBtn = true;
+            taskType = 5;
+            onTask();
+            sequentialSearch();
+        }
+    }
+
+    function binary(){
+        if(sorted == 0){
+            alert("정렬된 배열에서 탐색을 진행할 수 있도록 원소 변경 후, 최소 한 번의 정렬을 진행해주세요.");
+        }
+        else{
+            valueBtn = true;
+            taskType = 6;
+            onTask();
+            binarySearch();
+        }
+    }
+    
     function pause() {
         if (valueBtn === true) {
             sortPause();
         }
     }
 
-    const onSorting = async () => {
+    const onTask = async () => {
         var top = document.getElementById("top");
         var control = document.getElementById("control");
-        var sort;
+        var task;
 
-        switch (sortType) {
+        switch (taskType) {
             case 1: {
-                sort = document.getElementById("select");
+                task = document.getElementById("select");
                 break;
             }
             case 2: {
-                sort = document.getElementById("bubble");
+                task = document.getElementById("bubble");
                 break;
             }
             case 3: {
-                sort = document.getElementById("insert");
+                task = document.getElementById("insert");
                 break;
             }
             case 4: {
-                sort = document.getElementById("merge");
+                task = document.getElementById("merge");
+                break;
+            }
+            case 5: {
+                task = document.getElementById("sequential");
+                break;
+            }
+            case 6: {
+                task = document.getElementById("binary");
                 break;
             }
         }
-        sort.style.color = "black";
+        task.style.color = "black";
         top.style.backgroundColor = "lightgray";
         control.style.backgroundColor = "gray";
         while (valueBtn) {//on Sorting
             await sleep(100);
         }
-        sort.style.color = "white";
+        task.style.color = "white";
         top.style.backgroundColor = "#77E000";
         control.style.backgroundColor = "#1CD90B";
     }
@@ -172,13 +212,12 @@ export default function Header({ sortPause, sortRestart, setArray, updateList, r
 
                 <SpeedContainer>
                     <SpeedWrapper>
-                        <Typography id="discrete-slider-alwaysS" gutterBottom>
-                            {valueS}
+                        <Typography id="speedV" gutterBottom>
+                            3
                         </Typography>
                         <Slider
-                            value={typeof valueS === 'number' ? valueS : 0}
-                            onChange={handleSliderChangeSpeed}
-                            aria-labelledby="discrete-slider-alwaysS"
+                            value={valueS}
+                            onChange={SliderChange}
                             min={1}
                             max={5}
                         />
@@ -187,11 +226,15 @@ export default function Header({ sortPause, sortRestart, setArray, updateList, r
                 </SpeedContainer>
 
                 <SortWrapper>
-                    <SortButton id="select" onClick={select}>선택정렬</SortButton>
-                    <SortButton id="bubble" onClick={bubble}>버블정렬</SortButton>
-                    <SortButton id="insert" onClick={insert}>삽입정렬</SortButton>
-                    <SortButton id="merge" onClick={merge}>병합정렬</SortButton>
+                    <Button2 id="select" onClick={select}>선택정렬</Button2>
+                    <Button2 id="bubble" onClick={bubble}>버블정렬</Button2>
+                    <Button2 id="insert" onClick={insert}>삽입정렬</Button2>
+                    <Button2 id="merge" onClick={merge}>병합정렬</Button2>
                 </SortWrapper>
+                <SearchWrapper>
+                    <Button2 id="sequential" onClick={sequential}>순차탐색</Button2>
+                    <Button2 id="binary" onClick={binary}>이진탐색</Button2>
+                </SearchWrapper>
             </ Wrapper >
             <ControlWrapper id="control">
                 <Button onClick={buttonOff}>정렬 시작</Button>
@@ -207,6 +250,10 @@ export default function Header({ sortPause, sortRestart, setArray, updateList, r
                 <CountContainer>
                     병합횟수:
                     <Typography>{valueMC}</Typography>
+                </CountContainer>
+                <CountContainer>
+                    탐색횟수:
+                    <Typography>{valueSC}</Typography>
                 </CountContainer>
             </ControlWrapper>
         </Container>
