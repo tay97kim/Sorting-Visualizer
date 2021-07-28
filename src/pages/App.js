d5393a40db3d647de5f27c5dd74c2d2e3c5f6d9f
tsKIM <tay97kim@daegu.ac.kr>
2021-07-28 23:21:17 +0900

    Revert "Update App.js"
    This reverts commit a1d51989117c5a6be41c29c8f6c5b96d13d62d0b.

--- src/pages/App.js
+++ src/pages/App.js
@@ -584,13 +584,13 @@ function App() {
         setNowIdx(inputArr[i]);
         plusValue(4);
         setArr([...arr]);
-        await sleep(750/taskSpeed);
+        await sleep(375/taskSpeed);
         if(searchV==inputArr[i]){
           findIdx=i;
           break;
         }
       }
-      await sleep(500);
+      await sleep(375/taskSpeed);
       setCompIdx(inputArr[findIdx]);
       await sleep(500);
       setFreeIdx(inputArr[findIdx]);
